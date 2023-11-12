d959fe096926956e3b2429100911a80ed05ba69a
Jennifer Lynn Smith <jennifer@rocktownagency.com>
2023-11-12 16:27:55 -0500

    done

---
+++ index.js
@@ -0,0 +1,49 @@
+function add() {
+    // we'll fill this in shortly
+  }
+  // Addition Function
+function add(a, b) {
+    // Adding the value of a and b
+    return a + b;
+}
+
+// Subtraction Function
+function subtract(a, b) {
+    // Subtracting the value of b from a
+    return a - b;
+}
+
+// Multiplication Function
+function multiply(a, b) {
+    // Multiplying the value of a and b
+    return a * b;
+}
+
+// Division Function
+function divide(a, b) {
+    // Dividing the value of a by b
+    return a / b;
+}
+
+// Increment Function
+function increment(n) {
+    // Incrementing the value of n by 1
+    return n + 1;
+}
+
+// Decrement Function
+function decrement(n) {
+    // Decrementing the value of n by 1
+    return n - 1;
+}
+
+function makeInt(n) {
+    // Parsing the value of n as an integer in base 10
+    return parseInt(n, 10);
+}
+
+// Decimal Preservation Function
+function preserveDecimal(n) {
+    // Parsing the value of n as a floating-point number
+    return parseFloat(n);
+}
